9232e99426dde7ccae8a5bc9452c37d22e0cf39e
Ayache Khettar <ayache@zotix-consulting.co.uk>
2014-07-03 12:37:48 +0100

    adding release-example

---
+++ release-example/bundle-two/src/main/java/zoitx/maven/release/App.java
@@ -0,0 +1,13 @@
+package zoitx.maven.release;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
